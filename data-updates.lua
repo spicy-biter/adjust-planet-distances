c35a45a2fed9c78a533c27da4d99ee076ca4d254
Spicy Biter <spicybiter@gmail.com>
2025-01-05 23:44:48 -0500

    Create version 0.0.1

---
+++ data-updates.lua
@@ -0,0 +1,22 @@
+for temp, connection in pairs(data.raw["space-connection"]) do
+    -- aquilo routes
+    if connection.to == "aquilo" then
+        connection.length = math.floor(
+            connection.length
+            * settings.startup["planet-distances-outer-multiplier"].value
+        )
+    elseif connection.to == "solar-system-edge" then
+        -- aquilo to edge
+        connection.length = math.floor(
+            connection.length
+            * settings.startup["planet-distances-edge-multiplier"].value
+        )
+    else
+        -- inner planets
+        connection.length = math.floor(
+            connection.length
+            * settings.startup["planet-distances-inner-multiplier"].value
+        )
+    end
+end
+
---
+++ settings-updates.lua
@@ -0,0 +1,27 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "planet-distances-inner-multiplier",
+        setting_type = "startup",
+        default_value = 2.00,
+        minimum_value = 1.00,
+        maximum_value = 100,
+    },
+    {
+        type = "double-setting",
+        name = "planet-distances-outer-multiplier",
+        setting_type = "startup",
+        default_value = 2.00,
+        minimum_value = 1.00,
+        maximum_value = 100,
+    },
+    {
+        type = "double-setting",
+        name = "planet-distances-edge-multiplier",
+        setting_type = "startup",
+        default_value = 1.8,
+        minimum_value = 1.00,
+        maximum_value = 100,
+    },
+})
+
